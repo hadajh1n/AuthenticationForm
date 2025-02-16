c907242d9c9e68037f6a86d9bd1aae9881328a7f
hadajh1n <anton_sheludchenkov@mail.ru>
2025-02-16 22:08:11 +0300

    Обновление проекта: добавлена кнопка меню

--- app/src/main/java/com/example/authenticationform/UserActivity.java
+++ app/src/main/java/com/example/authenticationform/UserActivity.java
@@ -1,13 +1,21 @@
 package com.example.authenticationform;
 
+import com.example.authenticationform.R;
+import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.media.RouteListingPreference;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,18 +34,22 @@ public class UserActivity extends AppCompatActivity {
 
         TextView user_name = findViewById(R.id.user_name);
         TextView dbOutput = findViewById(R.id.dbOutput);
+
+        Button menuButton = findViewById(R.id.button_menu);
+        menuButton.setOnClickListener(this::showMenu);
+
         DatabaseHelper dbHelper = new DatabaseHelper(this);
 
         SharedPreferences preferences = getSharedPreferences("auth", MODE_PRIVATE);
         String userLogin = preferences.getString("USER_LOGIN", null);
 
-        if (userLogin.isEmpty()) {
+        if (userLogin == null || userLogin.isEmpty()) {
             startActivity(new Intent(this, MainActivity.class));
             finish();
             return;
         }
 
-        if(userLogin != null) {
+        if (userLogin != null) {
             String name = dbHelper.user_name(userLogin);
             user_name.setText("Привет, " + name + "!");
         } else {
@@ -45,15 +57,6 @@ public class UserActivity extends AppCompatActivity {
         }
 
         loadUserData(dbHelper, userLogin, dbOutput);
-
-        Button button_back = findViewById(R.id.button_back);
-        button_back.setOnClickListener(v -> {
-            preferences.edit().clear().apply();
-            Intent intent = new Intent(UserActivity.this, MainActivity.class);
-            startActivity(intent);
-            overridePendingTransition(0,0);
-            finish();
-        });
     }
 
     private void loadUserData(DatabaseHelper dbHelper, String login, TextView dbOutput) {
@@ -68,5 +71,39 @@ public class UserActivity extends AppCompatActivity {
         } else {
             dbOutput.setText("Ошибка загрузки данных...");
         }
+    }
+
+    private void showMenu(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(item -> {
+            handleClick(item);
+            return true;
+        });
+
+        popup.show();
+    }
+
+    private void handleClick(MenuItem item) {
+        int itemID = item.getItemId();
+
+        if (itemID == R.id.action_profile) {
+
+        } else if (itemID == R.id.action_settings) {
+
+        } else if (itemID == R.id.action_logout) {
+            logout();
+        }
+    }
+
+    private void logout() {
+        SharedPreferences preferences = getSharedPreferences("auth", MODE_PRIVATE);
+        preferences.edit().clear().apply();
+
+        Intent intent = new Intent(UserActivity.this, MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
     }
 }
